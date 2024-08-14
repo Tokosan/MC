5c510578d5e51f1731ce2640770a0ccd88a64522
Tokosan <cteiba.al@gmail.com>
2024-08-13 20:12:45 -0400

    downloading mods

--- admin/handtool.py
+++ admin/handtool.py
@@ -6,11 +6,17 @@ FILE = "files.json"
 with open(FILE) as f:
     data = json.load(f)
     for mod in data["mods"]:
-        url = mod["url"]
-        # descargamos los mods
-        wget.download(url)
-    for lib in data["libraries"]:
-        url = lib["url"]
-        # descargamos las librerias
-        wget.download(url)
+        name = mod["name"]
+        description = mod["description"]
+        print(f"{name}: {description}")
+        
+    # for mod in data["mods"]:
+    #     url = mod["url"]
+    #     # descargamos los mods
+    #     wget.download(url)
+    # for lib in data["libraries"]:
+    #     url = lib["url"]
+    #     # descargamos las librerias
+    #     wget.download(url)
+    
     
